1e289a6d2f6a16aae1932de9af810a7413133b07
ne1410s <pgj646@gmail.com>
2022-09-20 21:51:00 +0100

    Some initial groundwork

---
+++ source/Av.Tests/FileExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using Av.Models;
+
+namespace Av.Tests;
+
+/// <summary>
+/// Tests for the <see cref="FileExtensions"/>.
+/// </summary>
+public class FileExtensionsTests
+{
+    [Fact]
+    public void GetMediaTypeInfo_WithJpeg_ReturnsExpected()
+    {
+        // Arrange
+        var fi = new FileInfo("test.jpg");
+
+        // Act
+        var result = fi.GetMediaTypeInfo();
+
+        // Assert
+        result.Should().Be(MediaTypeInfo.Get("jpg"));
+    }
+}
