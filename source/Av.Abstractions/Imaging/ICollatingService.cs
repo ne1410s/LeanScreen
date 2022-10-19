505a15fc2f268f2bf8918e336c9c2bf906a70675
paul.jones <paul.jones@animalfriends.co.uk>
2022-10-19 16:39:27 +0100

    test

---
+++ source/Av.Abstractions/Imaging/ICollatingService.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="ICollatingService.cs" company="ne1410s">
+// Copyright (c) ne1410s. All rights reserved.
+// </copyright>
+
+namespace Av.Abstractions.Imaging
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Implementation for collating service.
+    /// </summary>
+    public interface ICollatingService
+    {
+    }
+}
