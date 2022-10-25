c1469979b974f388868a89ff1ce080b8f077f03f
ne1410s <pgj646@gmail.com>
2022-10-25 10:26:02 +0100

    Enumerate media

--- source/Av.Tests/FileExtensionsTests.cs
+++ source/Av.Tests/FileExtensionsTests.cs
@@ -24,4 +24,21 @@ public class FileExtensionsTests
         // Assert
         result.Should().Be(expected);
     }
+
+    [Theory]
+    [InlineData(MediaTypes.Image, 1)]
+    [InlineData(MediaTypes.Video, 1)]
+    [InlineData(MediaTypes.AnyMedia, 2)]
+    [InlineData(MediaTypes.NonMedia, 1)]
+    public void EnumerateMedia_WithFiles_ReturnsExpected(MediaTypes types, int expectedCount)
+    {
+        // Arrange
+        var di = new DirectoryInfo("Samples");
+
+        // Act
+        var media = di.EnumerateMedia(types);
+
+        // Assert
+        media.Count().Should().Be(expectedCount);
+    }
 }
--- source/Av/FileExtensions.cs
+++ source/Av/FileExtensions.cs
@@ -4,7 +4,9 @@
 
 namespace Av
 {
+    using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using Av.Models;
 
     /// <summary>
@@ -12,6 +14,8 @@ namespace Av
     /// </summary>
     public static class FileExtensions
     {
+        private const string AllFilesWildcard = "*";
+
         /// <summary>
         /// Gets media type information for the file.
         /// </summary>
@@ -19,5 +23,18 @@ namespace Av
         /// <returns>Media type information.</returns>
         public static MediaTypeInfo GetMediaTypeInfo(this FileInfo fi)
             => fi.Extension.GetMediaTypeInfo();
+
+        /// <summary>
+        /// Enumerates all media files recursively, according to the specified
+        /// media type(s).
+        /// </summary>
+        /// <param name="di">The root directory info.</param>
+        /// <param name="mediaTypes">The media type(s) to look for.</param>
+        /// <returns>A sequence of file paths.</returns>
+        public static IEnumerable<FileInfo> EnumerateMedia(
+            this DirectoryInfo di,
+            MediaTypes mediaTypes) => di
+                .EnumerateFiles(AllFilesWildcard, SearchOption.AllDirectories)
+                .Where(fi => mediaTypes.HasFlag(fi.GetMediaTypeInfo().MediaType));
     }
 }
