823adf7bb900e25ff67ad2aeddc0e98ec91d0abb
paul.jones <paul.jones@animalfriends.co.uk>
2023-02-02 10:27:04 +0000

    re-encr

--- source/AvCtl.Tests/InfoModuleTests.cs
+++ source/AvCtl.Tests/InfoModuleTests.cs
@@ -2,10 +2,11 @@
 // Copyright (c) ne1410s. All rights reserved.
 // </copyright>
 
+namespace AvCtl.Tests;
+
 using System.Collections.ObjectModel;
 using Comanche.Services;
-
-namespace AvCtl.Tests;
+using Crypt.IO;
 
 /// <summary>
 /// Tests for the <see cref="InfoModule"/>.
@@ -20,6 +21,11 @@ public class InfoModuleTests
     [InlineData("sample.mp4")]
     public void GetBasicInfo_SecureFile_ReturnsMessage(string fileName)
     {
+        //var file = new FileInfo(Path.Combine("Samples", "4a3a54004ec9482cb7225c2574b0f889291e8270b1c4d61dbc1ab8d9fef4c9e0.mp4"));
+        //var key = new byte[] { 26, 13, 129, 173, 11, 210, 216, 47, 15, 72, 217, 141, 124, 3, 238, 238, 97, 90, 73, 255 };
+        //var fs = File.OpenWrite("Samples/Info/dec.mp4");
+        //file.DecryptTo(fs, key);
+
         // Arrange
         var source = Path.Combine("Samples", fileName);
         var expected = File.ReadAllText(Path.Combine("Samples", "Info", fileName + ".json"));
@@ -29,7 +35,7 @@ public class InfoModuleTests
             .Returns(new Collection<string>(new[] { "123", "abc" }));
 
         // Act
-        var result = TestHelper.Route($"info basic -s {source} -ks . -kr .", mockWriter.Object);
+        var result = TestHelper.Route($"info basic -s {source} -ks Samples -kr xyz", mockWriter.Object);
 
         // Assert
         result.Should().Be(expected);
