6b5384b5fc487a17fcb3f3bdadbd6624dee3df43
ne1410s <pgj646@gmail.com>
2024-04-17 11:20:11 +0100

    fix tests

--- source/Av.MediaRepo.FileSystem.Tests/FileStoreTests.cs
+++ source/Av.MediaRepo.FileSystem.Tests/FileStoreTests.cs
@@ -5,6 +5,7 @@
 namespace Av.MediaRepo.FileSystem.Tests;
 
 using Av.MediaRepo.FileSystem;
+using Crypt.IO;
 
 /// <summary>
 /// Tests for the <see cref="FileStore"/> class.
@@ -65,5 +66,27 @@ public class FileStoreTests
         // Assert
         await act.Should().ThrowAsync<InvalidOperationException>()
             .WithMessage("Source name is not consistent with parent.");
+    }
+
+    [Fact]
+    public async Task NextUncapped_HasCounterpart_NotIncluded()
+    {
+        // Arrange
+        var dir = Guid.NewGuid().ToString();
+        new DirectoryInfo(dir).Create();
+        var fi = new FileInfo($"{dir}/{Guid.NewGuid()}.avi");
+        await File.WriteAllTextAsync(fi.FullName, "hello");
+        fi.EncryptInSitu([9, 0, 2, 1, 0]);
+        var capExt = fi.ToSecureExtension(".avi");
+        var capName = $"{fi.DirectoryName}/{fi.Name[..12]}.0123" + capExt;
+        await File.WriteAllTextAsync(capName, "world");
+        var sut = new FileStore(fi.DirectoryName!);
+
+        // Act
+        var todos = await sut.NextUncapped();
+
+        // Assert
+        todos.Should().BeEmpty();
+        Directory.Delete(fi.DirectoryName!, true);
     }
 }
