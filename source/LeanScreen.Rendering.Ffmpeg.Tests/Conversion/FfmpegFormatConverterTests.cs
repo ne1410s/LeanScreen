9fc75a69ee6e73bf82d2c35ad434fbd0f4fda5fb
ne1410s <pgj646@gmail.com>
2024-11-12 15:23:59 +0000

    Many more matches now!!

--- source/LeanScreen.Rendering.Ffmpeg.Tests/Conversion/FfmpegFormatConverterTests.cs
+++ source/LeanScreen.Rendering.Ffmpeg.Tests/Conversion/FfmpegFormatConverterTests.cs
@@ -15,11 +15,28 @@ using LeanScreen.Rendering.Ffmpeg.Conversion;
 public class FfmpegFormatConverterTests
 {
     [Theory]
-    [InlineData("sample2.avi", ".mp4")]
+    [InlineData("sample.avi", TargetExts.Asf)]
+    [InlineData("sample.avi", TargetExts.Mov)]
+    [InlineData("sample.avi", TargetExts.Mp4)]
+    [InlineData("sample.flv", TargetExts.Asf)]
+    [InlineData("sample.flv", TargetExts.Mov)]
+    [InlineData("sample.flv", TargetExts.Ts)]
+    [InlineData("sample.flv", TargetExts.Vob)]
+    [InlineData("sample.mkv", TargetExts.Asf)]
+    [InlineData("sample.mkv", TargetExts.Flv)]
+    [InlineData("sample.mkv", TargetExts.Mov)]
+    [InlineData("sample.mkv", TargetExts.Mp4)]
+    [InlineData("sample.mkv", TargetExts.Ts)]
+    [InlineData("sample.mkv", TargetExts.Vob)]
+    [InlineData("sample.mp4", TargetExts.Asf)]
+    [InlineData("sample.mp4", TargetExts.Flv)]
+    [InlineData("sample.mp4", TargetExts.Mov)]
+    [InlineData("sample.mp4", TargetExts.Ts)]
+    [InlineData("sample.mp4", TargetExts.Vob)]
     public void RemuxCryptoE2E_VaryingFile_MatchesDirect(string fileName, string ext)
     {
         // Obtain a control conversion by remuxing plain source via "direct mode"
-        var di = Directory.CreateDirectory($"{Guid.NewGuid()}");
+        var di = Directory.CreateDirectory($"{new FileInfo(fileName).Extension}2{ext}--{Guid.NewGuid()}");
         var srcFi = new FileInfo(Path.Combine(di.FullName, fileName));
         File.Copy(Path.Combine("Samples", fileName), srcFi.FullName);
         var controlFi = FfmpegFormatConverter.Remux(srcFi, ext, [], true);
@@ -37,161 +54,42 @@ public class FfmpegFormatConverterTests
         var controlHash = controlFi.Hash(HashType.Md5).Encode(Codec.ByteHex);
         var testingHash = testingFi.Hash(HashType.Md5).Encode(Codec.ByteHex);
 
-        using var controlFs = controlFi.OpenRead();
-        using var testingFs = testingFi.OpenRead();
+        testingHash.Should().Be(controlHash);
+        di.Delete(true);
 
-        var matchingBlocks = 0;
-        var buffer = new byte[32768];
-        var testBlocks = (int)Math.Ceiling((double)testingFi.Length / buffer.Length);
-        var badBoys = new List<int>();
-        var b1ControlBytes = "";
-        var b1TestingBytes = "";
+        ////using var controlFs = controlFi.OpenRead();
+        ////using var testingFs = testingFi.OpenRead();
 
-        for (var blockNo = 1; blockNo <= testBlocks; blockNo++)
-        {
-            Array.Clear(buffer);
-            var read = controlFs.Read(buffer, 0, buffer.Length);
-            var ctrl1 = buffer.AsSpan(0, read).ToArray().Hash(HashType.Md5).Encode(Codec.ByteHex);
-            if (blockNo == testBlocks) b1ControlBytes = string.Join("\r\n", buffer);
+        ////var matchingBlocks = 0;
+        ////var buffer = new byte[32768];
+        ////var testBlocks = (int)Math.Ceiling((double)testingFi.Length / buffer.Length);
+        ////var badBoys = new List<int>();
+        ////var b1ControlBytes = "";
+        ////var b1TestingBytes = "";
 
-            Array.Clear(buffer);
-            read = testingFs.Read(buffer, 0, buffer.Length);
-            var test = buffer.AsSpan(0, read).ToArray().Hash(HashType.Md5).Encode(Codec.ByteHex);
-            if (blockNo == testBlocks) b1TestingBytes = string.Join("\r\n", buffer);
+        ////for (var blockNo = 1; blockNo <= testBlocks; blockNo++)
+        ////{
+        ////    Array.Clear(buffer);
+        ////    var read = controlFs.Read(buffer, 0, buffer.Length);
+        ////    var ctrl1 = buffer.AsSpan(0, read).ToArray().Hash(HashType.Md5).Encode(Codec.ByteHex);
+        ////    if (blockNo == 1) b1ControlBytes = string.Join("\r\n", buffer);
 
-            if (ctrl1 == test)
-            {
-                matchingBlocks++;
-            }
-            else
-            {
-                badBoys.Add(blockNo);
-            }
-        }
+        ////    Array.Clear(buffer);
+        ////    read = testingFs.Read(buffer, 0, buffer.Length);
+        ////    var test = buffer.AsSpan(0, read).ToArray().Hash(HashType.Md5).Encode(Codec.ByteHex);
+        ////    if (blockNo == 1) b1TestingBytes = string.Join("\r\n", buffer);
 
-        matchingBlocks.Should().Be(testBlocks);
-    }
+        ////    if (ctrl1 == test)
+        ////    {
+        ////        matchingBlocks++;
+        ////    }
+        ////    else
+        ////    {
+        ////        badBoys.Add(blockNo);
+        ////    }
+        ////}
 
-    [Theory]
-    [InlineData(TargetExts.Asf)]
-    [InlineData(TargetExts.Flv)]
-    [InlineData(TargetExts.Mkv)]
-    [InlineData(TargetExts.Mov)]
-    [InlineData(TargetExts.Mp4)]
-    [InlineData(TargetExts.Ts)]
-    [InlineData(TargetExts.Vob)]
-    public void RemuxF2F_WhenCalled_ProducesExpected(string ext)
-    {
-        // Arrange
-        const string path = "C:\\temp\\~vids\\3.avi";
-
-        // Act
-        var result = FfmpegFormatConverter_001_File2File.Remux(path, ext);
-
-        // Assert
-        result.Should().Be(0);
-    }
-
-    [Theory]
-    ////[InlineData(TargetExts.Asf)]
-    ////[InlineData(TargetExts.Flv)]
-    ////[InlineData(TargetExts.Mkv)]
-    ////[InlineData(TargetExts.Mov)]
-    [InlineData(TargetExts.Mp4)]
-    ////[InlineData(TargetExts.Ts)]
-    ////[InlineData(TargetExts.Vob)]
-    public void RemuxV2VPlain_WhenCalled_ProducesExpected(string ext)
-    {
-        // Arrange
-        var source = new FileInfo("C:\\temp\\~vids\\3.avi");
-
-        // Act
-        var result = FfmpegFormatConverter.Remux(source, ext, []);
-
-        // Assert
-        result.Should().NotBeNull();
-    }
-
-    [Theory]
-    ////[InlineData(TargetExts.Asf)]
-    ////[InlineData(TargetExts.Flv)]
-    ////[InlineData(TargetExts.Mkv)]
-    ////[InlineData(TargetExts.Mov)]
-    [InlineData(TargetExts.Mp4)]
-    ////[InlineData(TargetExts.Ts)]
-    ////[InlineData(TargetExts.Vob)]
-    public void RemuxCB2CB_WhenCalled_ProducesExpected(string ext)
-    {
-        // Arrange
-        var key = new byte[] { 9, 0, 2, 1, 0 };
-        var source = new FileInfo(
-            "C:\\temp\\~vids\\e6d787f97a7b620cd438e75e3e5bdba384fe7df43605fad51eabb758fb347de5.1e99a40d6e");
-            ////"C:\\temp\\~vids\\edffa6fa556940cf3441b771c2cf62619b7207ada90ab54ff154598ad5705464.dc80231635");
-
-        // Act
-        var result = FfmpegFormatConverter.Remux(source, ext, key);
-
-        // Assert
-        result.Should().NotBeNull();
-    }
-
-    [Fact]
-    public void DecTest()
-    {
-        var key = new byte[] { 9, 0, 2, 1, 0 };
-        var src = new FileInfo(
-            "C:\\temp\\~vids\\out\\e6d787f97a7b620cd438e75e3e5bdba384fe7df43605fad51eabb758fb347de5.1e99a80b33");
-            ////"C:\\temp\\~vids\\out\\edffa6fa556940cf3441b771c2cf62619b7207ada90ab54ff154598ad5705464.dc802f1068");
-        src.DecryptHere(key);
-    }
-
-    [Theory]
-    [InlineData("1.avi.mp4")]
-    [InlineData("2.avi.mp4")]
-    public void HashTest(string suffix)
-    {
-        // Arrange
-        var controlRefFi = new FileInfo($"C:\\temp\\~vids\\out\\CONTROL__{suffix}");
-        var testingRefFi = new FileInfo($"C:\\temp\\~vids\\out\\TESTING__{suffix}");
-
-        var controlHash = controlRefFi.Hash(HashType.Md5).Encode(Codec.ByteHex);
-        var testingHash = testingRefFi.Hash(HashType.Md5).Encode(Codec.ByteHex);
-
-        ////testingHash.Should().Be(controlHash);
-
-        using var controlFi = controlRefFi.OpenRead();
-        using var testingFi = testingRefFi.OpenRead();
-
-        var matchingBlocks = 0;
-        var buffer = new byte[32768];
-        var testBlocks = (int)Math.Ceiling((double)testingRefFi.Length / buffer.Length);
-        var badBoys = new List<int>();
-        var b1ControlBytes = "";
-        var b1TestingBytes = "";
-
-        for (var blockNo = 1; blockNo <= testBlocks; blockNo++)
-        {
-            Array.Clear(buffer);
-            var read = controlFi.Read(buffer, 0, buffer.Length);
-            var ctrl1 = buffer.AsSpan(0, read).ToArray().Hash(HashType.Md5).Encode(Codec.ByteHex);
-            if (blockNo == testBlocks) b1ControlBytes = string.Join("\r\n", buffer);
-
-            Array.Clear(buffer);
-            read = testingFi.Read(buffer, 0, buffer.Length);
-            var test = buffer.AsSpan(0, read).ToArray().Hash(HashType.Md5).Encode(Codec.ByteHex);
-            if (blockNo == testBlocks) b1TestingBytes = string.Join("\r\n", buffer);
-
-            if (ctrl1 == test)
-            {
-                matchingBlocks++;
-            }
-            else
-            {
-                badBoys.Add(blockNo);
-            }
-        }
-
-        matchingBlocks.Should().Be(testBlocks);
+        ////matchingBlocks.Should().Be(testBlocks);
     }
 }
 
